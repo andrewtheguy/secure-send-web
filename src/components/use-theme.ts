818f66333b1b9bdd4a5c655854ca8cb4559791d0
Andrew Chen <andrew@jaanuu.com>
2025-12-20 00:35:25 -0800

    fix

--- src/components/use-theme.ts
+++ src/components/use-theme.ts
@@ -1,6 +1,6 @@
 import { useContext } from "react"
 
-import { ThemeProviderContext } from "./theme-provider"
+import { ThemeProviderContext } from "./theme-context"
 
 export const useTheme = () => {
   const context = useContext(ThemeProviderContext)
